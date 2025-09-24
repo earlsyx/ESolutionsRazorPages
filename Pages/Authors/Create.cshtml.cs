6c68cd07e592b18c73458909259a76c11432aa62
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-09-25 06:45:17 +0800

    bULDING fORMS WITH tAG hELPER
    tag
    <input>
    <div>
    <form>
    <a>
    <partial>
    <select>
    multi dropdow etc
    <envirnment>

--- Pages/Authors/Create.cshtml.cs
+++ Pages/Authors/Create.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ESolutionsRazorPages.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace ESolutionsRazorPages.Pages.Authors
 {
@@ -16,15 +18,23 @@ namespace ESolutionsRazorPages.Pages.Authors
         public CreateModel(ESolutionsRazorPages.Data.ApplicationDbContext context)
         {
             _context = context;
+            AllCourses = _context.course
+               .Select(c => new SelectListItem
+               {
+                   Value = c.Id.ToString(),
+                   Text = c.Name
+               }).ToList();
         }
 
         public IActionResult OnGet()
         {
+           
             return Page();
         }
 
         [BindProperty]
-        public Author Author { get; set; } = default!;
+        public CreateAuthorRequest InputModel { get; set; } = default!;
+        public List<SelectListItem> AllCourses { get; set; }
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
@@ -34,10 +44,44 @@ namespace ESolutionsRazorPages.Pages.Authors
                 return Page();
             }
 
-            _context.Authors.Add(Author);
+            var newAuthor = new Author
+            {
+                Name = InputModel.Name
+            };
+
+            _context.Authors.Add(newAuthor);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
         }
+
+        public class CreateAuthorRequest
+        {
+            [Required]
+            public string Name { get; set; } = default!;
+            [EmailAddress]
+            [Display(Name = "Email Address")]
+            public string EmailAddress { get; set; } = default!;
+            [Phone]
+            [Display(Name = "Phone Number")]
+            public string PhoneNumber { get; set; } = default!;
+            public bool IsActive { get; set; } = false;
+
+            [Display(Name = "Favorite Color")]
+            public string FavoriteColor { get; set; } = default!;
+            [Display(Name = "Courses Authored")]
+            public List<int> CoursesAuthored { get; set; } = new();
+            [Display(Name = "Date of Birth")]
+            public DateTime? DateOfBirth { get; set; } = default!;
+            [Display(Name = "Employment Type")]
+            public EmploymentType EmploymentType { get; set; } = default!;
+        }
     }
+}
+
+public enum EmploymentType
+{
+    FullTime,
+    PartTime,
+    Contractor
 }
